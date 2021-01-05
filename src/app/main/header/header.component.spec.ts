704aefb67839f6f7be81d8a96e6530d49c829e70
Thach Nguyen <nthachus@gmail.com>
2021-01-05 18:19:47 +0700

    Correct tests for main-header

--- src/app/main/header/header.component.spec.ts
+++ src/app/main/header/header.component.spec.ts
@@ -1,4 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { HeaderComponent } from './header.component';
 
@@ -8,6 +13,13 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule, //
+        FormsModule,
+        HttpClientModule,
+        TranslateModule.forRoot(),
+        JwtModule.forRoot({})
+      ],
       declarations: [HeaderComponent]
     }).compileComponents();
   }));
--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -1,4 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { MainComponent } from './main.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,6 +14,13 @@ describe('MainComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule, //
+        FormsModule,
+        HttpClientModule,
+        TranslateModule.forRoot(),
+        JwtModule.forRoot({})
+      ],
       declarations: [MainComponent, HeaderComponent]
     }).compileComponents();
   }));
