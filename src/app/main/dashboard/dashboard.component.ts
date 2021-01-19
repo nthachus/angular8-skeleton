7d1b9df2c6e85c427962d626bc8d52d99b26d666
Thach Nguyen <nthachus@gmail.com>
2021-01-19 19:41:45 +0700

    Import UploadX module

--- src/app/main/dashboard/dashboard.component.ts
+++ src/app/main/dashboard/dashboard.component.ts
@@ -1,11 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Tus, UploadAction, Uploader, UploadxOptions, UploadxService, UploadState } from 'ngx-uploadx';
+
+import { environment } from '../../../environments/environment';
+import { Logger } from '../../shared/logger';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  constructor() {}
+  readonly uploads$: Observable<Uploader[]>;
+
+  private uploadOptions: UploadxOptions = {
+    endpoint: `${environment.apiBaseUrl}upload`,
+    autoUpload: true,
+    chunkSize: 1048576, // 1MB
+    // retryConfig: { maxAttempts: 5 },
+    token: AuthService.currentToken as () => string, // Bearer token
+    uploaderClass: Tus
+  };
+
+  constructor(private uploadService: UploadxService) {
+    this.uploads$ = this.uploadService.connect(this.uploadOptions);
+  }
 
   ngOnInit(): void {}
+
+  onUploading(state: UploadState): void {
+    Logger.log(state);
+  }
+
+  processUpload(action: UploadAction, uploadId?: string): void {
+    this.uploadService.control({ action, uploadId });
+  }
 }
--- src/app/main/dashboard/dashboard.module.ts
+++ src/app/main/dashboard/dashboard.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UploadxModule } from 'ngx-uploadx';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
@@ -8,6 +9,7 @@ import { DashboardComponent } from './dashboard.component';
   declarations: [DashboardComponent],
   imports: [
     CommonModule, //
+    UploadxModule,
     DashboardRoutingModule
   ]
 })
